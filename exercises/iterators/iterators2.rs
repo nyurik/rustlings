536925add15410e38da46ebfbc7be1bfcc2c7459
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2023-01-11 17:07:55 -0500

    reverting to begining of iters

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -3,6 +3,8 @@
 // can offer. Follow the steps to complete the exercise.
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
 
+// I AM NOT DONE
+
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -10,7 +12,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.to_uppercase().to_string() + c.as_str(),
+        Some(first) => ???,
     }
 }
 
@@ -19,7 +21,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    words.iter().map(|word| capitalize_first(word)).collect()
+    vec![]
 }
 
 // Step 3.
@@ -27,7 +29,7 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    words.iter().map(|word| capitalize_first(word)).collect()
+    String::new()
 }
 
 #[cfg(test)]
